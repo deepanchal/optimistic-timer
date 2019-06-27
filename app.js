f359f561c01ab46839922ed369fa80f53cde212b
Deep Panchal <deep302001@gmail.com>
2019-06-26 20:29:59 -0500

    make it better

--- app.js
+++ app.js
@@ -1,51 +1,85 @@
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+let x, rangeVal = Number($('#customRange').val());
 document.getElementById('datePicker').valueAsDate = new Date();
-document.getElementById("timePicker").value = "23:59:59";
 
-let datePicker = new Date($('#datePicker').val());
-let timePicker = $('#timePicker').val();
-console.log(timePicker);
-let day = datePicker.getDate() + 1;
-let month = datePicker.getMonth() + 1;
-let year = datePicker.getFullYear();
-let hour = timePicker.getHours();
-let min = timePicker.getMinutes();
-
-console.log(day, month, year, hour, min);
+let countDownDate = new Date("Jun 28, 2019 23:59:59").getTime();
+// var countDownDate = new Date(2021, 1, 5, 15, 37, 25).getTime();
 
 $('#submit').on('click', function () {
-    $('.displayTime').css('display', 'block');
-    let countDownDate = new Date($('#datePicker').val());
-    let countDownTimer = new Date($('#timePicker').val());
-    console.log(countDownDate.getDate() + 1, countDownDate.getMonth() + 1, countDownDate.getHours(), countDownTimer.getHours(), );
-    
-    // setInterval(() => {
-    //     let now = new Date().getTime();
-    //     $('.displayTime').text(now);
-    // }, 1);
+    let datePicker = new Date($('#datePicker').val());
+    let timePicker = $('#timePicker').val();
+    //Very Important line for extracting input and creating a new date
+    //Total hours wasted: 20
+    countDownDate = new Date(`${monthNames[datePicker.getMonth()]} ${datePicker.getDate() + 1}, ${datePicker.getFullYear()} ${timePicker}`);
+
+    stopTimer();
+    startTimerFor(rangeVal);
+    $('#timerStats').css('display', 'block');
 });
 
-function startTimer() {
-    let show = setInterval(() => {
-        // Get today's date and time
-        var now = new Date().getTime();
+$('#customRange').on('input', function () {
+    stopTimer();
+    rangeVal = Number($('#customRange').val());
+    startTimerFor(rangeVal);
+});
 
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
+function startTimerFor(num) {
+    x = setInterval(() => {
+        const now = new Date().getTime();
+        const distance = countDownDate.getTime() - now;
+        // console.log(countDownDate, now, distance);
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        //Storing all values
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + (days * 24);
 
-        // Output the result in an element with id="demo"
-        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let maxMin = (hours * 60) + minutes;
 
-        // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(show);
-            document.getElementById("demo").innerHTML = "EXPIRED";
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let maxSeconds = (maxMin * 60) + seconds;
+
+        let milliseconds = Math.floor((distance % 1000));
+        let maxMilli = (maxSeconds * 1000) + milliseconds;
+
+        // let microseconds = maxMilli * 1000;
+
+        switch (num) {
+            case 1:
+                $('.displayTime').text(`${hours} hours left`);
+                break;
+            case 2:
+                $('.displayTime').text(`${maxMin} minutes left`);
+                break;
+            case 3:
+                $('.displayTime').text(`${maxSeconds} seconds left`);
+                break;
+            case 4:
+                $('.displayTime').text(`${maxMilli} milliseconds left`);
+                break;
+            case 5:
+                $('.displayTime').text(`${maxMilli}000 microseconds left`);
+                break;
+            case 6:
+                $('.displayTime').text(`${maxMilli}000000 nanoseconds left`);
+                break;
+            case 7:
+                $('.displayTime').text(`${maxMilli}000000000 picoseconds left`);
+                break;
+
+            default:
+                break;
         }
-    }, 1000);
+
+        if (distance < 0) {
+            clearInterval(x);
+            $('.displayTime').text('Times Up :(');
+        }
+    }, 1);
+}
+
+function stopTimer() {
+    clearInterval(x);
 }
